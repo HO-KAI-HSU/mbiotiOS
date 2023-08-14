47543a3e83f7dd46de58f6f3532685418857f560
george <george00157127@gmail.com>
2023-08-14 21:36:45 +0800

    Initial Commit

---
+++ SwiftProject2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftProject2
+//
+//  Created by georgeHsu on 2023/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftProject2/SwiftProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProject2App.swift
+//  SwiftProject2
+//
+//  Created by georgeHsu on 2023/8/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
